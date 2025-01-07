6e5a8296e039b91099f05d64823a6ce36478939f
ivozanklovandam <ivojankovic47@gmail.com>
2025-01-07 03:13:04 +0100

    Add files via upload

---
+++ zad2/zad3.asm
@@ -0,0 +1,48 @@
+@32000
+D = A
+@5
+M = D
+
+@0
+D = A
+@12
+M = D
+
+(LP)
+@12
+D = M
+@5
+D = D-A
+@ED
+D; JGE
+
+@12
+D = M
+@0
+A = A+D
+D = M
+
+@SK
+D; JLE
+
+@5
+A = M
+D = D-A
+@SK
+D; JGE
+
+@12
+D = M
+@0
+A = A+D
+D = M
+@5
+M = D
+
+(SK)
+@12
+M = M+1
+@LP
+0; JMP
+(ED)
+0; JMP
